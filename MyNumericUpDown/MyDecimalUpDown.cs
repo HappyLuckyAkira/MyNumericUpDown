6d6dc097a6070ea954e8c5e07bfbfe8561e2ab70
Akira <akira.kubo@konicaminolta.com>
2018-04-26 19:05:08 +0900

    ﻿:+1: add custom control
    Commit body...

--- MyNumericUpDown/MyDecimalUpDown.cs
+++ MyNumericUpDown/MyDecimalUpDown.cs
@@ -3,10 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Xceed.Wpf.Toolkit;
 
 namespace MyNumericUpDown
 {
-    class MyDecimalUpDown
+    class MyDecimalUpDown:DecimalUpDown
     {
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            DataObject.AddPastingHandler(this, textbox_PastingHandler);
+            var textBox = this.Template.FindName("PART_TextBox", this) as WatermarkTextBox;
+            if (null != textBox)
+            {
+                InputMethod.SetIsInputMethodSuspended(textBox, true);
+            }
+        }
+
+        private static bool IsAllNumber(string text)
+        {
+            return text.All(char.IsNumber);
+        }
+
+        protected override void OnPreviewTextInput(TextCompositionEventArgs e)
+        {
+            if (!IsAllNumber(e.Text))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private static void textbox_PastingHandler(object sender, DataObjectPastingEventArgs e)
+        {
+            if (e.DataObject.GetDataPresent(DataFormats.Text))
+            {
+                string text = Convert.ToString(e.DataObject.GetData(DataFormats.Text));
+                if (!IsAllNumber(text))
+                {
+                    e.CancelCommand();
+                }
+            }
+            else
+            {
+                e.CancelCommand();
+            }
+        }
     }
 }
