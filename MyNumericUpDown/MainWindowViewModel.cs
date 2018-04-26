0ceb1bc7e0aa0995284c285ff294fc8947bca099
Akira <akira.kubo@konicaminolta.com>
2018-04-26 19:34:06 +0900

    ﻿:+1: add Command & Property

--- MyNumericUpDown/MainWindowViewModel.cs
+++ MyNumericUpDown/MainWindowViewModel.cs
@@ -3,10 +3,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Prism.Mvvm;
+using Prism.Commands;
 
 namespace MyNumericUpDown
 {
-    class MainWindowViewModel
+
+    class MainWindowViewModel: BindableBase
     {
+        private int input;
+        private int input2;
+        bool hasViewError;
+        public bool HasViewError
+        {
+            get { return hasViewError; }
+            set
+            {
+                SetProperty(ref hasViewError, value);
+                SampleCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            this.SampleCommand = new DelegateCommand(
+                // Execute
+                () =>
+                {
+                },
+                // CanExecute
+                () =>
+                {
+                    return !HasViewError;
+                });
+        }
+        public DelegateCommand SampleCommand { get; private set; }
+
+        public int Input
+        {
+            get
+            {
+                return this.input;
+            }
+
+            set
+            {
+                SetProperty(ref input, value);
+            }
+        }
+
+        public int Input2
+        {
+            get
+            {
+                return this.input2;
+            }
+
+            set
+            {
+                SetProperty(ref input2, value);
+            }
+        }
     }
 }
